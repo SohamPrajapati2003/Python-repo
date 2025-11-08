49832d9d8342df47107d74e924c8c95c1f4959bc
SohamPrajapati2003 <156904822+SohamPrajapati2003@users.noreply.github.com>
2025-11-08 14:31:04 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,8 @@
 movies = [
-    {"movie_id": 1, "title": "Inception", "genre": "Sci-Fi", "rating": 8.8},
+    {"movie_id": 1, "title": "Avatar", "genre": "Sci-Fi", "rating": 8.8},
     {"movie_id": 2, "title": "Titanic", "genre": "Romance", "rating": 7.8},
     {"movie_id": 3, "title": "Avengers", "genre": "Action", "rating": 8.5},
-    {"movie_id": 4, "title": "Interstellar", "genre": "Sci-Fi", "rating": 8.6},
+    {"movie_id": 4, "title": "bahubali", "genre": "Sci-Fi", "rating": 8.6},
     {"movie_id": 5, "title": "The Notebook", "genre": "Romance", "rating": 7.9}
 ]
 
